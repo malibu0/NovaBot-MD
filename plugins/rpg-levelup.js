12ea40629a064e649d629753b38c883154f95979
The Lolibot-MD <107805973+elrebelde21@users.noreply.github.com>
2023-02-20 10:57:30 -0300

    Add files via upload

--- plugins/rpg-levelup.js
+++ plugins/rpg-levelup.js
@@ -1,59 +1,38 @@
-
 import { canLevelUp, xpRange } from '../lib/levelling.js'
+import { levelup } from '../lib/canvas.js'
+
 let handler = async (m, { conn }) => {
-	  let name = conn.getName(m.sender)
-    let pp = await conn.profilePictureUrl(m.sender, 'image').catch(_ => 'https://i.imgur.com/whjlJSf.jpg')
+	let name = conn.getName(m.sender)
     let user = global.db.data.users[m.sender]
-    if (!canLevelUp(user.level, user.exp, global.multiplier)) {
+    if (!canLevelUp(user.level, user.role, user.exp, global.multiplier)) {
         let { min, xp, max } = xpRange(user.level, global.multiplier)
-        let txt = `
-╭━─━─━─≪🔆≫─━─━─━╮
-│ *NIVEL*
-│◤━━━━━ ☆. ∆ .☆ ━━━━━◥
-│🔸Nombre : *${name}*
-│🔸Nivel : *${user.level}*
-│🔸XP : *${user.exp - min}/${xp}*
-│🔸Rango : *${user.role}*
+        throw `
+╭━─━─━─≪ *ɴɪᴠᴇʟ* ≫─━─━─━╮
+│🔸ɴᴏᴍʙʀᴇ : *${name}*
+│🔸ɴɪᴠᴇʟ : *${user.level}*
+│🔸xᴘ : *${user.exp - min}/${xp}*
+│🔸ʀᴀɴɢᴏ : *${user.role}*
 ╰━─━─━─≪🔆≫─━─━─━╯
 
-Te falta *${max - user.exp}* de *XP* para subir de nivel
+ᴛᴇ ғᴀʟᴛᴀ *${max - user.exp}* ᴅᴇ *XP* ᴘᴀʀᴀ sᴜʙɪʀ ᴅᴇ ɴɪᴠᴇʟ
 `.trim()
-try {
-  let imgg = API('fgmods', '/api/rank', {
-    username: name,
-    xp: user.exp - min,
-    exp: xp,
-    avatar: pp,
-    level: user.level,
-    background: 'https://i.ibb.co/CsNgBYw/qiyana.jpg'
-}, 'apikey')
-
-    conn.sendFile(m.chat, imgg, 'level.jpg', txt, m)
-} catch (e) {
-    m.reply(txt)
-}
     }
     let before = user.level * 1
     while (canLevelUp(user.level, user.exp, global.multiplier)) user.level++
     if (before !== user.level) {
-    	user.role = global.rpg.role(user.level).name
-
+        let teks = `🎊 ғᴇʟɪᴄɪᴅᴀᴅᴇs ${conn.getName(m.sender)}  ʟʟᴇɢᴀsᴛᴇ ᴀ ᴜɴ ɴᴜᴇᴠᴏ ɴɪᴠᴇʟ:`
         let str = `
-╭━─━─━─≪🔆≫─━─━─━╮
-│ *LEVEL UP*
-│◤━━━━━ ☆. ∆ .☆ ━━━━━◥
-│🔸Nivel anterior : *${before}*
-│🔸Nivel actual : *${user.level}*
+╭━─━─━─≪ *ʟᴇᴠᴇʟ ᴜᴘ* ≫─━─━─━╮
+│🔸ɴɪᴠᴇʟ ᴀɴᴛᴇʀɪᴏʀ : *${before}*
+│🔸ɴɪᴠᴇʟ ᴀᴄᴛᴜᴀʟ : *${user.level}*
 │🔸Rango : *${user.role}*
 ╰━─━─━─≪🔆≫─━─━─━╯
 
-*_Cuanto más interactúes con los bots, mayor será tu nivel_*
+*_ᴄᴜᴀɴᴛᴏ ᴍᴀs ɪɴᴛᴇʀᴀᴄᴛᴜᴇs ᴄᴏɴ ʟᴏs ʙᴏᴛs, ᴍᴀʏᴏʀ sᴇʀᴀ ᴛᴜ ɴɪᴠᴇʟ_*
 `.trim()
         try {
-            let img = API('fgmods', '/api/levelup', { 
-                avatar: pp 
-             }, 'apikey')
-      conn.sendFile(m.chat, img, 'levelup.jpg', str, m)
+            const img = await levelup(teks, user.level)
+            conn.sendFile(m.chat, img, 'levelup.jpg', str, m)
         } catch (e) {
             m.reply(str)
         }
@@ -62,6 +41,7 @@ try {
 
 handler.help = ['levelup']
 handler.tags = ['econ']
+
 handler.command = ['nivel', 'lvl', 'levelup', 'level'] 
 handler.register = true
 
