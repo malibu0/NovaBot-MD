631bc2014dfd504f3ff37fc8961945dfd75a5fea
LoliBot <107805973+elrebelde21@users.noreply.github.com>
2023-05-01 08:37:17 -0300

    NovaBot-MD

--- config.js
+++ config.js
@@ -3,7 +3,7 @@ import chalk from 'chalk'
 import { fileURLToPath } from 'url'
 
 global.owner = [
-  ['59822228462', 'NovaBot-MD', true],
+  ['59822228462', 'NovaBot-M', true],
   ['593968585383'],
   ['5214531173598'],
   ['50763596565'],
